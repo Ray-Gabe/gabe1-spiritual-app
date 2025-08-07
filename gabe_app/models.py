1b95eb1076013d452f5c26603d9f38a1739022ab
Ray-Gabe <raylin.sami@gmail.com>
2025-08-07 21:17:05 +0200

    Update models.py

--- gabe_app/models.py
+++ gabe_app/models.py
@@ -1,11 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
-from gabe_app import db
+db = SQLAlchemy()  # ✅ Define db here
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(256), nullable=False)
-
