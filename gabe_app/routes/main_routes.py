475c91f9b72d3b0b441cda92029502ea74f3bc3a
Ray-Gabe <raylin.sami@gmail.com>
2025-08-08 00:26:14 +0200

    Create main_routes.py

---
+++ gabe_app/routes/main_routes.py
@@ -0,0 +1,13 @@
+# gabe_app/routes/main_routes.py
+
+from flask import Blueprint, render_template
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/landing')
+def landing():
+    return render_template('landing.html')
