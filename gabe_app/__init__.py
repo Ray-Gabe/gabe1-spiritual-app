6a597e138e74681505a6e71551797143e2d3358f
Ray-Gabe <raylin.sami@gmail.com>
2025-08-07 21:12:25 +0200

    Update __init__.py

--- gabe_app/__init__.py
+++ gabe_app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from gabe_app.models import User  # 👈 Important
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -12,6 +13,11 @@ def create_app():
     db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_view = 'auth.login'
+
+    # 👇 Define the user_loader here
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     from gabe_app.routes.auth_routes import auth_bp
     from gabe_app.routes.main_routes import main_bp
