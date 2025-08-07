59acdd428639e3fb9c304ead6f55563703ef5f56
Ray-Gabe <raylin.sami@gmail.com>
2025-08-07 18:15:06 +0200

    Update __init__.py

--- gabe_app/__init__.py
+++ gabe_app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
@@ -9,11 +11,11 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
     db.init_app(app)
+    login_manager.init_app(app)
 
     from gabe_app.routes.auth_routes import auth_bp
-    from gabe_app.routes.main_routes import main_bp
-
     app.register_blueprint(auth_bp)
-    app.register_blueprint(main_bp)
 
     return app
+
+
